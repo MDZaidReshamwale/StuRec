260a8f98edab390527af88bb8bafd44f9f41c827
msre0001 <zaidreshamwale2015@gmail.com>
2021-12-21 08:46:57 +0530

    - Creating an floating button (add) in the activity_main.xml - Updated the onclick for float button in the MainActivity

---
+++ app/src/main/java/com/zaid/sturec/AddRecoordActivity.java
@@ -0,0 +1,14 @@
+package com.zaid.sturec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddRecoordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recoord);
+    }
+}
--- app/src/main/java/com/zaid/sturec/MainActivity.java
+++ app/src/main/java/com/zaid/sturec/MainActivity.java
@@ -3,12 +3,30 @@ package com.zaid.sturec;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+//    declaring float button
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Intialzing the float button
+        fab = findViewById(R.id.addFabBtn);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
 }
